60125b8606898823a670d282fcf28880a1f3050c
admalpan <admalpan@cisco.com>
2020-06-18 23:06:59 +0530

    docker

--- src/docker/main.go
+++ src/docker/main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(rs http.ResponseWriter, r *http.Request) {
-		rs.Write("hey this is container based go")
+		rs.Write([]byte("this is container based go"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
